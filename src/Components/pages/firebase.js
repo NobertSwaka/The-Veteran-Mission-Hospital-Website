ab47cbc325e1594a5a13d967dd0dbeeb95e53349
Lincolin Angaya <lincolinangaya5626@gmail.com>
2024-12-07 06:40:06 -0800

    new fire database working appropriately

--- src/Components/pages/firebase.js
+++ src/Components/pages/firebase.js
@@ -58,14 +58,12 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArSZ1HrISRXxYMDX23GPchwMIzCUx2wsY",
-  authDomain: "spotlessguru-ea64e.firebaseapp.com",
-  databaseURL: "https://spotlessguru-ea64e-default-rtdb.firebaseio.com",
-  projectId: "spotlessguru-ea64e",
-  storageBucket: "spotlessguru-ea64e.appspot.com",
-  messagingSenderId: "506667893385",
-  appId: "1:506667893385:web:f77cf4a229dfe32d0a7c58",
-  measurementId: "G-4ML0NF2VTV"
+  apiKey: "AIzaSyA6o3xSCDkEQ4EqGX9MGW1edtKCjbmjQUI",
+  authDomain: "veteranhospital-13b3b.firebaseapp.com",
+  projectId: "veteranhospital-13b3b",
+  storageBucket: "veteranhospital-13b3b.firebasestorage.app",
+  messagingSenderId: "457477143916",
+  appId: "1:457477143916:web:3b79f22a3bc470839c4749"
 };
 
 // Initialize Firebase
